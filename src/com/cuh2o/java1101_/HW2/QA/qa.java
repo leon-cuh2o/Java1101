4ebbd317d476def34d587dd8b79a48f4a942fe9a
Leon <leon.cuh2o@gmail.com>
2019-02-04 21:41:32 +0200

    New file was added using GitHub plugin via IntelliJ IDEA

--- src/com/cuh2o/java1101_/HW2/QA/qa.java
+++ src/com/cuh2o/java1101_/HW2/QA/qa.java
@@ -1,4 +1,19 @@
 package com.cuh2o.java1101_.HW2.QA;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class qa {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Давай знакомиться. Как тебя зовут? –> ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String Name = reader.readLine();
+        System.out.println("Приятно познакомиться, " +(String)Name + "\n");
+        if (Name.equals("Игорь") || Name.equals("Настя")) {
+            System.out.println("Иди нахуй, " +(String)Name + "\n");
+        }
+        else
+            if (Name.equals("Сиона")) {
+                System.out.println("Да здравствует ККК!");
+            }
+    }
 }
---
+++ src/com/cuh2o/java1101_/HelloWorld/HW2.java
@@ -0,0 +1,7 @@
+package com.cuh2o.java1101_.HW2;
+
+public class HW2 {
+    public static void main (String [] args) {
+        System.out.println("Hello world!");
+    }
+}
