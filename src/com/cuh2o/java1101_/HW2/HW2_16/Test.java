6e26f70faba7c13366ba98aa0c852cd658933ede
Leon <leon.cuh2o@gmail.com>
2019-02-04 21:37:53 +0200

    first commit

--- src/com/cuh2o/java1101_/HW2/HW2_16/Test.java
+++
@@ -1,7 +0,0 @@
-package com.cuh2o.java1101_.HW2.HW2_16;
-
-public class Test {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/com/cuh2o/java1101_/HW2/QA/qa.java
@@ -0,0 +1,4 @@
+package com.cuh2o.java1101_.HW2.QA;
+
+public class qa {
+}
